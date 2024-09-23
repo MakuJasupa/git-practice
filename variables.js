501eb0348365b4656ed0281ce0997d1b6a6cf0b7
Jasu <101542776+MakuJasupa@users.noreply.github.com>
2024-09-23 10:42:31 +0800

    Completed Variables section

---
+++ variables.js
@@ -0,0 +1,10 @@
+//String variables
+let name = "Mark Jasper Gacutan";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Male";
+let twitterHandle = "@maku_jasu";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
