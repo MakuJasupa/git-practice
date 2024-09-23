4305505bd0ab9c83c78d520215ceb73b96c16549
Jasu <101542776+MakuJasupa@users.noreply.github.com>
2024-09-23 11:44:21 +0800

    Completed Comparison section

---
+++ comparison.js
@@ -0,0 +1,11 @@
+let firstFavNumb = 6;
+let secondFavNumb = 9;
+
+console.log(6 > 9);
+console.log(6 < 9);
+console.log(6 >= 9);
+console.log(6 <= 9);
+console.log(6 === 9);
+console.log(6 == 9);
+console.log(6 !== 9);
+console.log(6 != 9);
