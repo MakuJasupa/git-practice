a18057f8e0a14df8bde122cad3c34497695ec3ea
Jasu <101542776+MakuJasupa@users.noreply.github.com>
2024-09-23 12:01:10 +0800

    Completed Strings section

---
+++ strings.js
@@ -0,0 +1,7 @@
+let favActorFirstName = "Tom";
+let favActorLastName = "Cruise";
+let fullName = favActorFirstName.concat(" ", favActorLastName);
+let uppercase = fullName.toUpperCase();
+let message = ("My favorite actor is " + uppercase + " his best show is Silicon Valley");
+
+console.log(message);
