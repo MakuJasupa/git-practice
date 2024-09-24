baf18dde1adb5242c5560d2ea4aaf4cbb9e3558f
Jasu <101542776+MakuJasupa@users.noreply.github.com>
2024-09-24 21:58:15 +0800

    Completed C1-A1 Activity

---
+++ CS2D_JasperGacutan_C1-A1.js
@@ -0,0 +1,41 @@
+//Persona Data Storage
+
+//Set up variables
+
+//Persona 1
+let P1_firstName = "Mark Jasper";
+let P1_MiddleName = "Celino";
+let P1_LastName = "Gacutan";
+let P1_FullName = (P1_firstName + " " + P1_MiddleName + " " + P1_LastName);
+let P1_BirthDate = "February 28, 2002";
+let P1_BirthPlace = "San Juan, Sta. Lucia, Philippines, 2710";
+let P1_Address = "Calaoa-an, Candon City, Ilocos Sur, 2710";
+let P1_CourseYear = "BSCS 2";
+let P1_DreamJob = "Game Development";
+
+//Persona 2
+let P2_firstName = "Mark";
+let P2_MiddleName = "Jester";
+let P2_LastName = "Gacutan";
+let P2_FullName = (P2_firstName + " " + P2_MiddleName + " " + P2_LastName);
+let P2_BirthDate = "February 28, 2002";
+let P2_BirthPlace = "San Juan, Sta. Lucia, Philippines, 2710";
+let P2_Address = "Calaoa-an, Candon City, Ilocos Sur, 2710";
+let P2_CourseYear = "BSCS 2";
+let P2_DreamJob = "Game Tester";
+
+//Persona 3
+let P3_firstName = "Micah";
+let P3_MiddleName = "Jewel";
+let P3_LastName = "Gacutan";
+let P3_FullName = (P3_firstName + " " + P3_MiddleName + " " + P3_LastName);
+let P3_BirthDate = "February 21, 1999";
+let P3_BirthPlace = "Candon, Philippines, 2710";
+let P3_Address = "Calaoa-an, Candon City, Ilocos Sur, 2710";
+let P3_CourseYear = "BSCS 2";
+let P3_DreamJob = "Software Engineer";
+
+//Print out variables
+console.log(P1_FullName.toUpperCase() + " was born " + P1_BirthDate + " at " + P1_BirthPlace.toUpperCase() + ", and currently living at " + P1_Address.toUpperCase() + ". " + P1_FullName.toLowerCase() + " is taking up " + P1_CourseYear + " and dreams to be " + P1_DreamJob + " after graduation.");
+console.log(P2_FullName.toUpperCase() + " was born " + P2_BirthDate + " at " + P2_BirthPlace.toUpperCase() + ", and currently living at " + P2_Address.toUpperCase() + ". " + P2_FullName.toLowerCase() + " is taking up " + P2_CourseYear + " and dreams to be " + P2_DreamJob + " after graduation.");
+console.log(P3_FullName.toUpperCase() + " was born " + P3_BirthDate + " at " + P3_BirthPlace.toUpperCase() + ", and currently living at " + P3_Address.toUpperCase() + ". " + P3_FullName.toLowerCase() + " is taking up " + P3_CourseYear + " and dreams to be " + P3_DreamJob + " after graduation.");
