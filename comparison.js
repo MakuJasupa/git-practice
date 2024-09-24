b134401d58a6ad774b14ace1f1776e8f29d48aaf
Jasu <101542776+MakuJasupa@users.noreply.github.com>
2024-09-24 19:53:09 +0800

    Completed Comparison section

--- comparison.js
+++ comparison.js
@@ -1,11 +1,11 @@
 let firstFavNumb = 6;
 let secondFavNumb = 9;
 
-console.log(6 > 9);
-console.log(6 < 9);
-console.log(6 >= 9);
-console.log(6 <= 9);
-console.log(6 === 9);
-console.log(6 == 9);
-console.log(6 !== 9);
-console.log(6 != 9);
+console.log(firstFavNumb > secondFavNumb);
+console.log(firstFavNumb < secondFavNumb);
+console.log(firstFavNumb >= secondFavNumb);
+console.log(firstFavNumb <= secondFavNumb);
+console.log(firstFavNumb === secondFavNumb);
+console.log(firstFavNumb == secondFavNumb);
+console.log(firstFavNumb !== secondFavNumb);
+console.log(firstFavNumb != secondFavNumb);
