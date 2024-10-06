4c2a479c3b1b587577ab979f1fa4441ab73d1b2e
Jasu <101542776+MakuJasupa@users.noreply.github.com>
2024-10-06 21:34:35 +0800

    Completed Iteration Mapping with Double Loops

---
+++ iterationMapping.js
@@ -0,0 +1,20 @@
+// Iteration Mapping
+
+// Ask for user input for the limit of the double loop
+let maxLimit = parseInt(prompt("Enter the Limit of the Double Loop: "));
+
+// Variables for double loop 
+let firstCounter = 0;
+let secondCounter = 0;
+let addedValue = 0;
+
+// Double loop
+while (secondCounter < maxLimit) {
+  firstCounter = 0;
+  while (firstCounter < maxLimit) {
+    addedValue = secondCounter + firstCounter;
+    console.log(`[${secondCounter}] [${firstCounter}] Added value is ${addedValue}`);
+    firstCounter++;
+  }
+  secondCounter++;
+}
