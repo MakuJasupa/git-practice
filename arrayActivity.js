3370aa186931334a8dd55f9a54d181937b54a6ca
Jasu <101542776+MakuJasupa@users.noreply.github.com>
2024-10-16 21:00:53 +0800

    Array Activity

---
+++ arrayActivity.js
@@ -0,0 +1,22 @@
+// Working with Arrays
+
+// Create an array called favSingers and store your three favorite singers in it.
+let favSingers = ["Tatsuro Yamashita", "The Weekend", "Lil Uzi Vert"];
+
+// Log the first singer in the array using array index notation.
+console.log(favSingers[0]); // Output: Tatsuro Yamashita
+
+// Create an array called favNumbers and store four of your favorite numbers.
+let favNumbers = [69, 28, 0, 1];
+
+// Create an array named mixedArr and store different data types.
+let mixedArr = ["string", ["other array"], 123, true];
+
+// Access and log each item in the mixedArr using array index notation.
+console.log(mixedArr[0]); // Output: string
+console.log(mixedArr[1]); // Output: ["other array"]
+console.log(mixedArr[2]); // Output: 123
+console.log(mixedArr[3]); // Output: true
+
+// Access and log the first item in the nested array.
+console.log(mixedArr[1][0]); // Output: other array
